780129a1c2f3ae4a1835f95121ecbafa35a57737
Matthew Roloff <matthewroloff@gmail.com>
2021-05-15 19:55:44 -0600

    first commit

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ class App{
             
             this.children[0].scale.z = 10;
             this.userData.selectPressed = true;
-            this.rightHeld.add(this.highlight)
+            //this.rightHeld.add(this.highlight)
         }
 
         function onSelectEnd() {
